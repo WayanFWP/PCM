e465d25e2253e64fd8b9e2ab3388d41c953a49cc
WayanFWP <piwayan1@gmail.com>
2026-04-14 04:55:48 -0700

    feat: ultrasound segmentation ✅, extraction feature ✅
    todo: make it robust to all segmented data(still using data that have contrast on head and uterus)

--- Assigment/module/preprocessing.py
+++ Assigment/module/preprocessing.py
@@ -1,6 +1,10 @@
 import imageio
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools
+import scipy.ndimage as ndi
+
+from skimage import exposure
 
 def CNR(inside, outside):
     mean_in  = np.mean(inside)
@@ -43,17 +47,70 @@ def MSE(label, original, processed):
     
     return mse
     
-def NM(original, processed):
+def NM(label, original, processed):
     M_Orig = np.mean(original)
     M_Pro = np.mean(processed)
     
     nm = M_Pro / M_Orig
     
-    print(f"NM : {nm}")
+    print(f"{label}-NM : {nm}")
     
     return nm
     
 def AnalysisPreROI(label, original, proccessed):
-    PSNR(label, original, proccessed)
-    MSE(label, original, proccessed)
+    psnr = PSNR(label, original, proccessed)
+    mse = MSE(label, original, proccessed)
+    nm = NM(label, original, proccessed)
     
+    return psnr, mse, nm
+
+    
+class Analysis:
+    def __init__(self):
+        self.records = []
+        self.result = []
+    
+    def findOptimalEnhancement(self, image, methode, parameter_space):
+        original = image.astype(np.float64)
+
+        sigma, clip_limits, median_sizes = parameter_space
+        
+        records = gaussian(original, )
+        
+        df = pd.DataFrame(records)
+
+        df_sorted = df.sort_values(by="PSNR", ascending=False)
+
+        print(df_sorted.head(10))
+
+        top_n = 6
+        top_indices = df_sorted.head(top_n).index
+
+        plt.figure(figsize=(12,6))
+        for i, idx in enumerate(top_indices):
+            name, img = results[idx]
+            plt.subplot(2, 3, i+1)
+            plt.imshow(img, cmap='gray')
+            plt.title(name)
+            plt.axis('off')
+
+        plt.suptitle("Top Enhancement Results (by PSNR)")
+        plt.tight_layout()
+        plt.show()
+
+    def gaussian(self, im=None, sigma=1, eq=False, clip_limits=0.01):
+        filtering = ndi.gaussian_filter(im, sigma=sigma)
+        if eq == True and clip_limits > 0:
+            enhanced = exposure.equalize_adapthist(filtering, clip_limits=clip_limits)
+        elif eq == True and clip_limits == None:
+            enhanced = exposure.equalize_hist(filtering)
+            
+        psnr, mse, nm = AnalysisPreROI("Gaussian", im, filtering)
+        
+        return self.records.append({
+            "Method": "Gaussian+CLAHE",
+            "Sigma": sigma,
+            "Median": None,
+            "MSE": mse,
+            "PSNR": psnr
+        })
